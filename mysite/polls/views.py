e6702654cb7fbc5e40a0be8ce306bfc90b445c26
michaeltmarion <michaelmarion@icloud.com>
2015-04-19 19:42:27 -0400

    Fix templates, remove .pyc files.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,7 +8,7 @@ from polls.models import Question
 
 def index(request):
 	latest_question_list = Question.objects.order_by("-pub_date")[:5]
-	template = loader.get_template('polls/index.html')
+	template = loader.get_template('index.html')
 	context = RequestContext(request, {
 		'latest_question_list': latest_question_list,
 		})
